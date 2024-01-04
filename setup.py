1aa09056006c68fbb637f1c4c854c24b57a09e44
Shubham <agrawalshubh932@gmail.com>
2024-01-04 19:24:34 +0530

    Project set up for house price prediction app.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='In this project I am building a house price prediction app for end users.',
+    author='Shubham Agrawal',
+    license='',
+)
